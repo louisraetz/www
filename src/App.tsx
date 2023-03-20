ee109c7a7c5224cf31c2f48aec6fe0ffc79ade74
Louis Raetz <louis@louisraetz.com>
2023-03-20 19:23:38 +0100

    added some seo stuff, played around with document title manipulation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { globalStyles } from '~designSystem/theme'
 
 const App = () => {
   globalStyles()
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    /* TODO: Add functionality later to track command which got entered into the shell */
+    let title = 'louisraetz@macbook:~ ▌'
+    const interval = setInterval(() => {
+      if (title.length === 22) {
+        title = title.substring(0, title.length - 1)
+      } else {
+        title += '▌'
+      }
+
+      document.title = title
+    }, 500)
+
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <div className="App">
       <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount(c => c + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
