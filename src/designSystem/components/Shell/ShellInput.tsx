280541c3f15c5b4ebff47efc6d9dc3095b0658b4
Louis Raetz <louis@louisraetz.com>
2023-03-21 19:00:48 +0100

    prepare tabulator auto complete support

--- src/designSystem/components/Shell/ShellInput.tsx
+++ src/designSystem/components/Shell/ShellInput.tsx
@@ -1,6 +1,12 @@
 /// <reference types="vite-plugin-svgr/client" />
 
-import React, { useState, FocusEvent, KeyboardEvent, useRef } from 'react'
+import React, {
+  useState,
+  FocusEvent,
+  KeyboardEvent,
+  useRef,
+  useEffect,
+} from 'react'
 import { styled } from '@stitches/react'
 import Fuse from 'fuse.js'
 import FocusTrap from 'focus-trap-react'
@@ -33,12 +39,30 @@ const CMDGhostInput = styled('input', {
   opacity: 0,
 })
 
+const Binary = styled('p', {
+  variants: {
+    active: {
+      true: {
+        background: '#fff',
+      },
+    },
+  },
+})
+
 const ShellInput = () => {
   const inputRef = useRef<HTMLInputElement | null>(null)
   const { sendCMD, memory } = useShell()
   const [value, setValue] = useState('')
   const [memoryCounter, setMemoryCounter] = useState<number>(0)
   const [matches, setMatches] = useState<string[]>([])
+  const [matchCounter, setMatchCounter] = useState<number>(0)
+
+  useEffect(() => {
+    if (value.length === 0) {
+      setMatchCounter(0)
+      setMatches([])
+    }
+  }, [value])
 
   function handleKeyPress(e: KeyboardEvent<HTMLInputElement>) {
     const { key, target } = e
@@ -53,8 +77,16 @@ const ShellInput = () => {
       const fuse = new Fuse(mockFiles, options)
       const possibleMatches = fuse.search(val)
       setMatches(possibleMatches.map(fuseMatch => fuseMatch.item))
-      if (possibleMatches[0].item != null) {
-        setValue(possibleMatches[0].item)
+      if (possibleMatches[matchCounter].item != null) {
+        if (val !== possibleMatches[matchCounter].item) {
+          setValue(possibleMatches[matchCounter].item)
+          setMatchCounter(p => {
+            if (p === possibleMatches.length - 1) {
+              return 0
+            }
+            return p + 1
+          })
+        }
       }
     }
 
@@ -98,8 +130,13 @@ const ShellInput = () => {
             onBlur={(e: FocusEvent<HTMLInputElement>) => e.target.focus()}
           />
         </Flex>
-        <Flex>
-          {matches.length > 0 && matches.map(match => <p>{match}</p>)}
+        <Flex gap="2">
+          {matches.length > 0 &&
+            matches.map((match, i) => (
+              <Binary active={i === matchCounter} className="binary">
+                {match}
+              </Binary>
+            ))}
         </Flex>
       </Flex>
     </FocusTrap>
