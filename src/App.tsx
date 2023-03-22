732e36f72540d94f6cffd895b35726f8d363651f
Louis Raetz <louis@louisraetz.com>
2023-03-22 17:06:32 +0100

    active asset caching

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {
 import Bootscreen from '~screens/Bootscreen'
 import Shell from '~designSystem/components/Shell'
 import { ShellProvider } from '~lib/shellContext'
+import Flex from '~designSystem/components/Flex'
 
 export enum Theme {
   MAC = 'macos',
@@ -27,6 +28,7 @@ const App = () => {
    * forgive me
    * */
   const [theme, setTheme] = useState<Theme | undefined>(undefined)
+  const [loading, setLoading] = useState<boolean>(true)
 
   globalStyles()
 
@@ -43,11 +45,41 @@ const App = () => {
       document.title = title
     }, 350)
 
+    const cacheImages = async (srcArray: string[]) => {
+      const promises = srcArray.map(src => {
+        return new Promise(function (resolve, reject) {
+          const img = new Image()
+
+          img.src = src
+          img.onload = resolve('') as any // don't bother
+          img.onerror = reject() as any // don't bother
+        })
+      })
+
+      await Promise.all(promises)
+
+      setLoading(false)
+    }
+
+    const imgs = [
+      '/images/background-linux.jpg',
+      '/images/background-macos.jpg',
+    ]
+
+    cacheImages(imgs)
+
     return () => clearInterval(interval)
   }, [])
 
   return (
     <Body className={theme === Theme.LINUX ? linuxTheme.toString() : ''}>
+      {loading && (
+        <Flex direction="column" css={{ position: 'fixed', top: 24, left: 24 }}>
+          {/* TBD */}
+          <p>Disabling selection.</p>
+          <p>Loading assets... [0/2]</p>
+        </Flex>
+      )}
       {theme != null ? (
         <ShellProvider>
           <Shell t={theme} />
